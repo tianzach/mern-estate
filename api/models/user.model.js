5df2ce87c66e0ccf85c26f49f4e643ea99c82b49
zactian <zition21043@icloud.com>
2024-11-11 13:51:01 -0800

    create update user api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar:{
         type: String,
-        default: "https://www.google.com/imgres?q=profile&imgurl=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2015%2F10%2F05%2F22%2F37%2Fblank-profile-picture-973460_1280.png&imgrefurl=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&docid=wg0CyFWNfK7o5M&tbnid=ycNOFIKv7gjqcM&vet=12ahUKEwik59DBhNOJAxXCJzQIHcX7EWcQM3oECBoQAA..i&w=1280&h=1280&hcb=2&ved=2ahUKEwik59DBhNOJAxXCJzQIHcX7EWcQM3oECBoQAA"
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
     },
 },
     {timestamps: true});
