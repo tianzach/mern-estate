5416110195c4e4831a0dd2c65d730c95fed8387b
zactian <zition21043@icloud.com>
2024-11-11 14:58:03 -0800

    complete update user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, current } from "@reduxjs/toolkit";
+import { updateUser } from "../../../../api/controllers/user.controller";
 
 const initialState = {
     currentUser: null,
@@ -22,9 +23,21 @@ const userSlice = createSlice({
             state.error = action.payload;
             state.loading = false;
         },
+        updateUserStart:(state) =>{
+            state.loading = true;
+        },
+        updateUserSuccess:(state, action) =>{
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null; 
+        },
+        updateUserFailure:(state, action) =>{
+            state.error = action.payload;
+            state.loading = false;
+        }
     }
 
 });
 
-export const {signInStart, signInSuccess, signInFailure} = userSlice.actions;
+export const {signInStart, signInSuccess, signInFailure, updateUserStart, updateUserSuccess, updateUserFailure} = userSlice.actions;
 export default userSlice.reducer;
