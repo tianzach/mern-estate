2e06f16a0817c8f6db0c1493ff48e8c468dd8ab9
zactian <zition21043@icloud.com>
2024-11-17 13:01:19 -0800

    complete delete user listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,3 +1,4 @@
+import errorHandler from 'errorhandler';
 import Listing from '../models/listing.model.js';
 
 export const createListing = async (req, res, next) => {
@@ -5,6 +6,23 @@ export const createListing = async (req, res, next) => {
         const listing = await Listing.create(req.body);
         return res.status(201).json(listing);
         
+    } catch (error) {
+        next(error);
+        
+    }
+}
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if(!listing) {
+        return next(errorHandler(404, 'Listing not found!'))
+    }
+    if(req.user.id !== listing.userRef) {
+        return next(errorHandler(403, 'You are not authorized to delete this listing'));
+    }
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: 'Listing deleted successfully' });
     } catch (error) {
         next(error);
         
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { createListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
+import { deleteListing } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken,createListing);
+
+router.delete('/delete/:id', verifyToken, deleteListing);
+
 export default router;
