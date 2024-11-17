3b83b6a69c7093aeb5ac76e0107ebfffaed5e4c9
zactian <zition21043@icloud.com>
2024-11-17 13:49:45 -0800

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,30 +1,51 @@
-import errorHandler from 'errorhandler';
-import Listing from '../models/listing.model.js';
+import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
-    try {
-        const listing = await Listing.create(req.body);
-        return res.status(201).json(listing);
-        
-    } catch (error) {
-        next(error);
-        
-    }
-}
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const deleteListing = async (req, res, next) => {
-    const listing = await Listing.findById(req.params.id);
-    if(!listing) {
-        return next(errorHandler(404, 'Listing not found!'))
-    }
-    if(req.user.id !== listing.userRef) {
-        return next(errorHandler(403, 'You are not authorized to delete this listing'));
-    }
-    try {
-        await Listing.findByIdAndDelete(req.params.id);
-        res.status(200).json({ message: 'Listing deleted successfully' });
-    } catch (error) {
-        next(error);
-        
-    }
-}
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(
+      errorHandler(401, "You are not authorized to delete this listing")
+    );
+  }
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Listing deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(
+      errorHandler(401, "You are not authorized to update this listing")
+    );
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,11 +2,14 @@ import express from 'express';
 import { createListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 import { deleteListing } from '../controllers/listing.controller.js';
+import { updateListing } from '../controllers/listing.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken,createListing);
 
 router.delete('/delete/:id', verifyToken, deleteListing);
+
+router.post('/update/:id', verifyToken, updateListing);
 
 export default router;
